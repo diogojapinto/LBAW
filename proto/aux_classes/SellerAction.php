4586a09b395e2052f396e2ac96d089c9e25a1f70
m3t4ph03n1x <diogojapinto@gmail.com>
2014-06-06 13:30:32 +0100

    i hate so much php, it turns out that sizeof is an alias of count, but the last one works and the other dont

---
+++ proto/aux_classes/SellerAction.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: knoweat
+ * Date: 06/06/14
+ * Time: 13:04
+ */
+include_once("../config/init.php");
+include_once($BASE_DIR + "database/negotiation.php");
+
+class SellerAction extends Thread {
+    private $idDeal;
+
+    function __construct($idDeal) {
+        $this->idDeal = $idDeal;
+    }
+
+
+    public function run(){
+        $waitTime = rand(60, 60 * 5);
+        sleep($waitTime);
+        sellerAction($this->idDeal);
+    }
+} 
